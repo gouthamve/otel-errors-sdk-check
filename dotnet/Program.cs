4c98b3e82a626308c59b38bb2ab343466d587379
Liudmila Molkova <neskazu@gmail.com>
2026-01-15 16:59:52 -0800

    Partial fixes on ruby, show logging integration on .net and java

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -7,4 +7,18 @@ app.MapGet("/throw-error", () => {
     throw new InvalidOperationException("Intentional error from .NET ASP.NET Core application");
 });
 
+app.MapGet("/manual-error", (ILogger<Program> logger) => {
+    try
+    {
+        throw new InvalidOperationException("Intentional error from .NET ASP.NET Core application");
+    }
+    catch (InvalidOperationException e)
+    {
+        // that's similar to how ASP.NET logs controller errors
+        logger.LogError(e, "Caught an intentional error");
+        return "we handled the error";
+    }
+});
+
+
 app.Run();
--- ruby/app.rb
+++ ruby/app.rb
@@ -2,30 +2,46 @@ require 'sinatra'
 require 'opentelemetry/sdk'
 require 'opentelemetry/exporter/otlp'
 require 'opentelemetry/instrumentation/all'
+require 'opentelemetry-logs-sdk'
+require 'opentelemetry-instrumentation-logger'
+require 'opentelemetry-exporter-otlp-logs'
+require 'logger'
 
 # Configure OpenTelemetry Traces
 OpenTelemetry::SDK.configure do |c|
   c.use_all
 end
+at_exit do
+  OpenTelemetry.logger_provider.shutdown
+end
 
 # Note: Ruby OTEL Logs SDK is experimental and unstable.
-# Logs are written to stdout and can be collected via container logging.
-# Traces and exception capture work via auto-instrumentation.
+# it also does not works well with sinatra
 
 set :bind, '0.0.0.0'
 set :port, 8080
 
+$logger = Logger.new(STDOUT)
+# logging works here
+$logger.info("$logger configured")
+begin
+  raise StandardError, 'Intentional error from startup'
+rescue StandardError => e
+  $logger.error(e)
+end
+
 get '/' do
-  puts "[INFO] Received request to root endpoint"
+  # this doesn't work - logs are printed to console, but not captured by Otel - looks like a bug in otel?
+  $logger.info("Handling root endpoint")
   'Hello from Ruby Sinatra!'
 end
 
 get '/throw-error' do
-  puts "[ERROR] About to throw an intentional error"
   raise StandardError, 'Intentional error from Ruby Sinatra application'
 end
 
 error StandardError do
-  puts "[ERROR] Unhandled error: #{env['sinatra.error'].message}"
+  # does not work with otel
+  $logger.error(env['sinatra.error'])
   "Error: #{env['sinatra.error'].message}"
 end
